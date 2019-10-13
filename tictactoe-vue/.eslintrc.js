321d93a6e7c80b0f08e9a9316ac7d45b37acbe84
jonas <jonas.ohrn@cygni.se>
2019-10-13 20:38:23 +0200

    Removed eslint airbnb

--- tictactoe-vue/.eslintrc.js
+++ tictactoe-vue/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es6: true,
     browser: true,
   },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
